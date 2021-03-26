94929141ed523c9a3588158eb188bf2126accb0a
kunal7722 <dev35.programmingtree@gmail.com>
2021-03-26 14:58:25 +0530

    Empty main branch

--- Js/script.js
+++
@@ -1,8 +0,0 @@
-const sideNav = document.querySelector(".sidenav");
-const btnToggle = document.querySelector(".btn-toggle");
-
-function showSideNav() {
-  sideNav.classList.toggle("active");
-}
-
-btnToggle.addEventListener("click", showSideNav);
